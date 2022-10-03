0e553eb30f5c038fb9e5bbc4af9518bb439148bb
Mojens <moha135h@stud.kea.dk>
2022-10-03 09:56:31 +0200

    navigation router

---
+++ index.js
@@ -0,0 +1,46 @@
+import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
+
+
+import {
+  setActiveLink, adjustForMissingHash, renderTemplate, loadHtml
+} from "./utils.js"
+
+window.addEventListener("load", async () => {
+
+  const templateAbout = await loadHtml("./pages/about/about.html")
+  const templateNotFound = await loadHtml("./pages/notFound/notFound.html")
+
+  adjustForMissingHash()
+
+  const router = new Navigo("/", { hash: true });
+  //Not especially nice, BUT MEANT to simplify things. Make the router global so it can be accessed from all js-files
+  window.router = router
+
+  router
+    .hooks({
+      before(done, match) {
+        setActiveLink("menu", match.url)
+        done()
+      }
+    })
+    .on({
+      //For very simple "templates", you can just insert your HTML directly like below
+      "/": () => document.getElementById("content").innerHTML =
+        `<h2>Home</h2>
+      <p style='margin-top:2em'>
+      This is the content of the Home Route
+      </p>
+     `,
+      "/about": () => renderTemplate(templateAbout, "content")
+    })
+    .notFound(() => {
+      renderTemplate(templateNotFound, "content")
+    })
+    .resolve()
+});
+
+
+window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+  alert('Error: ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber
+    + ' Column: ' + column + ' StackTrace: ' + errorObj);
+}
