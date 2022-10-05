8534714e28929ed97accd06652aabaceeb833297
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-10-05 14:11:29 +0200

    add select input for movies and theater

--- pages/screenings/addScreening/addScreening.js
+++ pages/screenings/addScreening/addScreening.js
@@ -6,18 +6,18 @@ let screening = [];
 
 export function initAddScreening(navigoRouter) {
   document.getElementById("addScreening").onclick = addScreening;
+  populateMovieSelect();
+  populateTheaterSelect();
   router = navigoRouter;
 }
 
 async function addScreening() {
-  const movieId = document.getElementById("if3").value;
-  const theaterId = document.getElementById("if4").value;
   const startTime = document.getElementById("if1").value;
   const endTime = document.getElementById("if2").value;
-
+  const movieId = document.getElementById("if3").value;
+  const theaterId = document.getElementById("if4").value;
 
   const newScreening = {
-    
     startTime,
     endTime,
     movieId,
@@ -36,4 +36,28 @@ async function addScreening() {
   router.navigate(`screening?id=${id}`);
 }
 
+// function for populating select options for movie and theater
+async function populateMovieSelect() {
+    const url = "http://localhost:8080/api/movies";
+    const movies = await fetch(url).then((res) => res.json());
+    const select = document.getElementById("if3");
+    movies.forEach((movie) => {
+      const option = document.createElement("option");
+      option.value = movie.id;
+      option.innerHTML = movie.title;
+      select.appendChild(option);
+    });
+}
 
+// function for populating select options for theater
+async function populateTheaterSelect() {
+    const url = "http://localhost:8080/api/theaters";
+    const theaters = await fetch(url).then((res) => res.json());
+    const select = document.getElementById("if4");
+    theaters.forEach((theater) => {
+      const option = document.createElement("option");
+      option.value = theater.id;
+      option.innerHTML = "Sal " + theater.id;
+      select.appendChild(option);
+    });
+}
