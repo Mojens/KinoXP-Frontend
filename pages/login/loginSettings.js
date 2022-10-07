842e309c6cc9ec0c313cd06f83ed7aee83b85216
Mojens <moha135h@stud.kea.dk>
2022-10-07 12:13:03 +0200

    sessionBoth

--- pages/login/loginSettings.js
+++ pages/login/loginSettings.js
@@ -60,6 +60,44 @@ export function checkSession2() {
     }
 }
 
+export function checkSessionBoth() {
+    if (sessionStorage.getItem("user2") !== null || sessionStorage.getItem("user1") !== null) {
+        const boxes = document.querySelectorAll('.sessionCheck3');
+        boxes.forEach(box => {
+            box.style.display = "inline-block";
+        });
+    }
+    else if (sessionStorage.getItem("user2") === null || sessionStorage.getItem("user1") === null) {
+
+        const boxes = document.querySelectorAll('.sessionCheck3');
+        boxes.forEach(box => {
+            box.style.display = "none";
+        });
+    } else if (sessionStorage.getItem("user2") !== null && sessionStorage.getItem("user1") == null) {
+
+        const boxes = document.querySelectorAll('.sessionCheck2');
+        boxes.forEach(box => {
+            box.style.display = "inline-block";
+        });
+
+        const boxes2 = document.querySelectorAll('.sessionCheck1');
+        boxes2.forEach(box2 => {
+            box2.style.display = "none";
+        });
+    }else if (sessionStorage.getItem("user1") !== null && sessionStorage.getItem("user2") == null) {
+
+        const boxes = document.querySelectorAll('.sessionCheck1');
+        boxes.forEach(box => {
+            box.style.display = "inline-block";
+        });
+        const boxes2 = document.querySelectorAll('.sessionCheck2');
+        boxes2.forEach(box2 => {
+            box2.style.display = "none";
+
+        });
+    }
+}
+
 export function removeSession() {
     if (sessionStorage.getItem("user1") != null) {
         sessionStorage.removeItem("user1");
@@ -78,6 +116,7 @@ export function startSession(type) {
 export function changeLoginText() {
     checkSession1();
     checkSession2();
+    checkSessionBoth();
     if (sessionStorage.getItem("user1") != null || sessionStorage.getItem("user2") != null) {
         document.getElementById("loginNav1").innerText = "Logout";
     } else if (sessionStorage.getItem("user1") == null || sessionStorage.getItem("user2") == null) {
