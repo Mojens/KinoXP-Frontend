c1a8a5e88900e7850f81f686d0e0832e5868277f
Johannes-Forsting <johannes.forsting@gmail.com>
2022-10-20 17:31:00 +0200

    tilføjet kommentarer

--- settings.js
+++ settings.js
@@ -1,3 +1,5 @@
+//const url = "https://localhost:8080"
+
 export let URL_EMPLOYEES = "https://forzacinema.azurewebsites.net/api/employees/"
 export let URL_LOGIN = "https://forzacinema.azurewebsites.net/api/employees/login/"
 export let URL_MOVIES = "https://forzacinema.azurewebsites.net/api/movies/"
@@ -9,3 +11,18 @@ export let URL_SEATS_THEATER_ID = "https://forzacinema.azurewebsites.net/api/sea
 export let URL_RESERVATIONS_FROM_SCREENING = "https://forzacinema.azurewebsites.net/api/reservations/fromScreening/"
 export let URL_SEAT_CHOICES_LIST = "https://forzacinema.azurewebsites.net/api/seatchoices/list"
 export let URL_SCREENINGS_ALL = "https://forzacinema.azurewebsites.net/api/screenings/all"
+
+
+/*
+export let URL_EMPLOYEES = "https://forzacinema.azurewebsites.net/api/employees/"
+export let URL_LOGIN = "https://forzacinema.azurewebsites.net/api/employees/login/"
+export let URL_MOVIES = "https://forzacinema.azurewebsites.net/api/movies/"
+export let URL_RESERVATIONS = "https://forzacinema.azurewebsites.net/api/reservations/"
+export let URL_SCREENINGS = "https://forzacinema.azurewebsites.net/api/screenings/"
+export let URL_SHIFTS = "https://forzacinema.azurewebsites.net/api/shifts/"
+export let URL_THEATERS = "https://forzacinema.azurewebsites.net/api/theaters/"
+export let URL_SEATS_THEATER_ID = "https://forzacinema.azurewebsites.net/api/seats/theaterid/"
+export let URL_RESERVATIONS_FROM_SCREENING = "https://forzacinema.azurewebsites.net/api/reservations/fromScreening/"
+export let URL_SEAT_CHOICES_LIST = "https://forzacinema.azurewebsites.net/api/seatchoices/list"
+export let URL_SCREENINGS_ALL = "https://forzacinema.azurewebsites.net/api/screenings/all"
+*/
